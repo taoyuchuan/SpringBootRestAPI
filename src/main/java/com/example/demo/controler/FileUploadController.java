cb38cc86af7be1d8f8b51ad5c80fd46bba909a53
taoyuchuan <taoyuchuan@gmail.com>
2020-12-26 15:33:29 -0500

    Add file upload function

---
+++ src/main/java/com/example/demo/controler/FileUploadController.java
@@ -0,0 +1,34 @@
+package com.example.demo.controler;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * Class to upload files.
+ */
+@RestController
+public class FileUploadController {
+    /**
+     * Upload a file.
+     * @param file File to upload
+     * @return Response message;
+     * @throws IOException When failed.
+     */
+    @RequestMapping(value = "/upload", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String fileUpload(@RequestParam("file") MultipartFile file) throws IOException {
+        File convertFile = new File("/var/tmp/"+file.getOriginalFilename());
+        convertFile.createNewFile();
+        FileOutputStream fout = new FileOutputStream(convertFile);
+        fout.write(file.getBytes());
+        fout.close();
+        return "File is upload successfully";
+    }
+}
