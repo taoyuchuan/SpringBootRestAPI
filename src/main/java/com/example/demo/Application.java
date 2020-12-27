2d40e310b8878ea3da10abf0abbc36265eebc982
taoyuchuan <taoyuchuan@gmail.com>
2020-12-26 23:09:14 -0500

    Add CORS support

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * Driver class.
@@ -16,6 +19,20 @@ public class Application {
 	 */
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	/**
+	 * Set the CORS configuration globally.
+	 * @return Global configuration of CORS.
+	 */
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/products").allowedOrigins("http://localhost:8080");
+			}
+		};
 	}
 
 	/**
--- src/main/java/com/example/demo/controler/ProductServiceController.java
+++ src/main/java/com/example/demo/controler/ProductServiceController.java
@@ -4,11 +4,7 @@ import com.example.demo.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.demo.model.Product;
 
@@ -49,6 +45,7 @@ public class ProductServiceController {
      * @return response message.
      */
     @RequestMapping(value = "/products", method = RequestMethod.POST)
+    @CrossOrigin(origins = "http://localhost:8080")
     public ResponseEntity<Object> createProduct(@RequestBody Product product) {
         productService.createProduct(product);
         return new ResponseEntity<>("Product is created successfully", HttpStatus.CREATED);
@@ -59,6 +56,7 @@ public class ProductServiceController {
      * @return all products.
      */
     @RequestMapping(value = "/products")
+    @CrossOrigin(origins = "http://localhost:8080")
     public ResponseEntity<Object> getProduct() {
         return new ResponseEntity<>(productService.getProducts(), HttpStatus.OK);
     }
