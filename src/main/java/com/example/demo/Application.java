072f74c99e7f2a62944250a8ced6388cd731062c
taoyuchuan <taoyuchuan@gmail.com>
2021-01-09 20:01:31 -0500

    Enable Swagger2 - Create UI to access REST APIs

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -8,10 +8,16 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 /**
  * Driver class.
  */
 @SpringBootApplication
+@EnableSwagger2
 public class Application {
 	/**
 	 * Driver function.
@@ -42,5 +48,15 @@ public class Application {
 	@Bean
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
+	}
+
+	/**
+	 * Get APIs information from controllers.
+	 * @return APIs information.
+	 */
+	@Bean
+	public Docket productApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.example.demo")).build();
 	}
 }
